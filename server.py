5e1432bd3680ed25dfc5fa7f42680a994c6e9872
moppermonster <26716125+moppermonster@users.noreply.github.com>
2025-03-18 11:17:55 +0100

    restructured data storage and visualisation

--- server.py
+++ server.py
@@ -1,79 +1,182 @@
+'''
+check-in terminal server
+check-in terminal client talks to this server to keep track of what
+users have (not) checked in.
+'''
+
+
 from datetime import datetime
 
-from flask import Flask, request
-
-app = Flask('app')
-
-db = {}
+from flask import Flask, request, jsonify
 
 
-def date_check():
-    date = datetime.now().strftime('%Y %m %d')
+class Userdata:
+    def __init__(self, group):
+        self.group = group
+        self.check_in_time = None
 
-    for entry in db:
-        if entry != date:
-            db.pop(entry)
 
-    if date not in db:
-        with open('users.txt') as _f:
+class Database:
+    def __init__(self):
+
+        # Set the database date
+        self.date =  datetime.now().strftime('%Y %m %d')
+
+        # Configure from users.txt
+        group = None
+        with open('users.txt', 'r') as _f:
             lines = _f.readlines()
-        ids = []
+        ids = {}
+        groups = []
         for line in lines:
+            if line.startswith('#'):
+                continue
             line = line.rsplit('\n')[0]
-            ids.append(line)
+            if not line:
+                continue
+            if line.startswith('@'):
+                line = line[1:]
+                if not line.strip():
+                    continue
+                group = line
+                if not group in groups:
+                    groups.append(group)
+                continue
+            if not group:
+                group = 'No group set'
+                if not group in groups:
+                    groups.append(group)
+            ids.update({
+                line: Userdata(group)
+            })
 
-        db.update({date: {'checked_in': {}, 'not_checked_in': ids, 'unknown': {}}})
-    return date
+        self.groups = groups
+        self.groups.sort()
+
+        sorted_ids = {}
+        for _id in sorted(ids):
+            sorted_ids.update({_id: ids[_id]})
+        self.ids = sorted_ids
+
+        # Set up a fallback group for unknown identities
+        self.unknown = {}
+
+
+app = Flask('cit server')
+app.db = Database()
 
 
 @app.route('/')
-def index():
-    date = date_check()
-    html_head = '<html><head><title>checkin</title></head><body>'
-    html_tail = '</body></html>'
-    body = []
-    body.append('<h2>Checked in</h2>')
-    for entry in db[date]['checked_in']:
-        body.append(f'<p> {db[date]["checked_in"][entry]} : {entry}</p>')
-    body.append('<h2>Not checked in</h2>')
-    for _id in db[date]['not_checked_in']:
-        body.append(f'<p>{_id}</p>')
+@app.route('/<mode>')
+def index(mode='html'):
 
-    if db[date]['unknown']:
-        body.append('<h2>Unknown IDs</h2>')
-        for entry in db[date]['unknown']:
-            body.append(f'<p>{db[date]["unknown"][entry]} : {entry}</p>')
+    data = {}
+    for group in app.db.groups:
+        data.update({group: []})
+
+    if mode == 'json':
+        for group in app.db.groups:
+            data.update({group: {}})
+        for user in app.db.ids:
+            data[app.db.ids[user].group].update({user: app.db.ids[user].check_in_time})
+        return jsonify(data)
+    else:
+        for group in app.db.groups:
+            data.update({group: []})
+        for user in app.db.ids:
+            data[app.db.ids[user].group].append((user, app.db.ids[user].check_in_time))
+
+    html_head = '''<!doctype HTML>
+<html>
+    <head>
+    <title>Check-in terminal</title>
+    <style>
+    html {
+    font-family: sans-serif;
+    }
+    table {
+    background: #333333;
+    color: white;
+    }
+    td {
+    color: #000000;
+    }
+    #tables {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: flex-start;
+    align-content: center;
+    }
+    .not_checked_in {
+    background-color: #FFCCCC;
+    }
+    .checked_in {
+    background-color: #DAF7A6;
+    }
+    </style>
+    </head>
+<body>'''
+    html_tail = '</body></html>'
+
+
+
+    body = []
+    body.append(f'<h2>{app.db.date}</h2>')
+    body.append('<div id="tables">')
+
+    for group in data:
+        body.append('<table>')
+        body.append(f'<tr><th>{group}</th></tr>')
+        for user in data[group]:
+            markup = 'class="not_checked_in"'
+            if user[1]:
+                markup = 'class="checked_in"'
+            body.append(f'<tr {markup}><td>{user[0]}</td><td>{user[1]}</td></tr>')
+        body.append('</table>')
+
+    if app.db.unknown:
+        body.append('<table>')
+        body.append(f'<tr><th>Unknown entries</th></tr>')
+        for user in app.db.unknown:
+            body.append(f'<tr class="checked_in"><td>{user}</td><td>{app.db.unknown[user]}</td></tr>')
+        body.append('</table>')
+
+    body.append('</div>')
 
     body = '\n'.join(body)
 
     return f'{html_head}\n{body}\n{html_tail}'
 
+
 @app.route('/listen', methods=['POST'])
 def listen():
-    date = date_check()
+
     json_dict = request.get_json(force=True)
-    if "user" not in json_dict:
+
+    if 'user' not in json_dict:
         return "Missing required key 'user'", 400
-    if "timestamp" not in json_dict:
+
+    if 'timestamp' not in json_dict:
         return "Missing required key 'timestamp'", 400
+
     user = json_dict['user']
     timestamp = json_dict['timestamp']
-    if user not in db[date]['checked_in']:
-        if user in db[date]['not_checked_in']:
-            print(user)
-            db[date]['not_checked_in'].remove(user)
-            db[date]['checked_in'].update({user: timestamp})
-        else:
-            db[date]['unknown'].update({user: timestamp})
 
-    return "Accepted", 200
+    if user in app.db.ids:
+        if not app.db.ids[user]['check_in_time']:
+            app.db.ids[user]['check_in_time'] = timestamp
+    else:
+        if user not in app.db.unknown:
+            app.db.unknown.update({user: timestamp})
+
+    return 'Accepted', 200
 
 
 if __name__ == '__main__':
-    _ = date_check()
-    print(db)
     app.run(
             debug=True,
-            host='0.0.0.0',
+            host='127.0.0.1',
             port=5003,
             )
