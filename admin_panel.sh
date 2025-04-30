acfb730a7ba2181737607832808dea552d139554
moppermonster <26716125+moppermonster@users.noreply.github.com>
2025-04-30 14:44:48 +0200

    added admin panel

--- admin_panel.sh
+++ admin_panel.sh
@@ -3,5 +3,14 @@
 password="$(yad --center --width:400 --entry --hide-text --title="Login required" --button=gtk-ok:0 --button=gtk-cancel:1)"
 
 if [[ "$password" == "$(< ~/password.txt)" ]]; then
-    xfce4-terminal
+    yad --center --width:400 --title="CIT Admin" \
+        --button="Close"\!gtk-close\!"Close this window":0 \
+        --button="Copy to USB"\!drive-harddisk-usb-symbolic\!"Copy CIT data to a USB flash drive":2 \
+        --button="Terminal"\!\!"Close this window":3
+    if [[ "$?" == "2" ]]; then
+        bash /cit/usb_copy.sh
+    fi
+    if [[ "$?" == "3" ]]; then
+        xfce4-terminal
+    fi
 fi
