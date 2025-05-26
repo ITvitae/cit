b9c65cfc9af9079aa0d2775f7ec4d54f22f310f7
moppermonster <26716125+moppermonster@users.noreply.github.com>
2025-05-26 14:44:53 +0200

    added View button to see data on the machine from admin panel

--- admin_panel.sh
+++ admin_panel.sh
@@ -6,12 +6,16 @@ if [[ "$password" == "$(< ~/password.txt)" ]]; then
     yad --center --width:400 --title="CIT Admin" \
         --button="Close"\!gtk-close\!"Close this window":0 \
         --button="Copy to USB"\!drive-harddisk-usb-symbolic\!"Copy CIT data to a USB flash drive":2 \
-        --button="Terminal"\!\!"Close this window":3
+        --button="Terminal"\!\!"Close this window":3 \
+        --button="View"\!\!"View today's CIT file":4
     result="$?"
     if [[ "$result" == "2" ]]; then
         bash /cit/usb_copy.sh
     fi
     if [[ "$result" == "3" ]]; then
         xfce4-terminal
+    fi
+    if [[ "$result" == "4" ]]; then
+        xfce4-terminal -e "less $HOME/cit/$(date +"%Y_%m_%d").scv"
     fi
 fi
